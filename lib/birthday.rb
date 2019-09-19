3b9599859d26a54390bd9345bd52d0e75617577c
Srikalyan Megati <kalmegati@gmail.com>
2019-09-19 02:16:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,7 +2,7 @@ birthday_kids = { "Timmy" => 9, "Sarah" => 6, "Amanda" => 27 }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each { |kid, age|
-    puts "Happy Birthday #{kid}! You are now #{age} years old!" if age <= 13
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
   }
 end
 
