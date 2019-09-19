947d0412c8d3f972a0ca123eea08ce7cfab06040
Srikalyan Megati <kalmegati@gmail.com>
2019-09-19 02:12:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 birthdays = { "Timmy" => 9, "Sarah" => 6, "Amanda" => 27 }
 
-def happy_birthday(birthday_kids = { "Timmy" => 9, "Sarah" => 6, "Amanda" => 27 })
+def happy_birthday(birthday_kids)
   happy_birthday.each { |kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old!" }
 end
 
